87c94801b4289a50960deecf8e6d0d73572d9826
Kamil Strózik <struzikk@gmail.com>
2015-04-09 10:36:14 +0200

    Dodanie menu

--- src/main/java/pl/edu/pk/pkCodeTeam1/biblioteka/App.java
+++ src/main/java/pl/edu/pk/pkCodeTeam1/biblioteka/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "pa" );
+        System.out.println( "Menu" );
+        System.out.println("1. katalog");
     }
 }
