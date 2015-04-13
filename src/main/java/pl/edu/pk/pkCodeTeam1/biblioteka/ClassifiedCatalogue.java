1e2a36b7c1596f98febde44ec4a30c8cdf2ce5a6
MikeNovember <niwa.mateusz@gmail.com>
2015-04-13 14:00:30 +0200

    Dodanie metod add_book i print.

--- src/main/java/pl/edu/pk/pkCodeTeam1/biblioteka/ClassifiedCatalogue.java
+++ src/main/java/pl/edu/pk/pkCodeTeam1/biblioteka/ClassifiedCatalogue.java
@@ -10,10 +10,23 @@ package pl.edu.pk.pkCodeTeam1.biblioteka;
  */
 public class ClassifiedCatalogue {
 	
+	public void add_book(String category, int ID){
+		Browsable temp = m_category_hierarchy.search(category);
+		
+		if (temp.is_book())
+			return;	//moznaby tez rzucac wyjatek ?
+		
+		((Category)temp).add_book(ID);
+	}
+	
+	public void print(String category){
+		System.out.println(find(category).contents_string());		
+	}
+	
 	public Browsable find(String category){
 		return m_category_hierarchy.search(category);
 	}
 	
-	private Browsable m_category_hierarchy;
+	private Category m_category_hierarchy;
 
 }
