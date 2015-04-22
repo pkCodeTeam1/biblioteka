7e7049a831f0f85817891cfd41f5d38dfac14868
MikeNovember <niwa.mateusz@gmail.com>
2015-04-22 19:32:24 +0200

    Ostateczne zmiany.

--- src/main/java/pl/edu/pk/pkCodeTeam1/biblioteka/Category.java
+++ src/main/java/pl/edu/pk/pkCodeTeam1/biblioteka/Category.java
@@ -19,12 +19,14 @@ public class Category implements Browsable{
 	public String contents_string(){
 		StringBuilder temp = new StringBuilder();
 		
+		temp.append(m_name + "\n");
+		
 		for(int i = 0 ; i < m_subcategories.size() ; ++i){
 			temp.append(i);
 			temp.append(". ");
 			
 			if (m_subcategories.get(i).is_book())
-				temp.append(((BookWrapper)m_subcategories.get(i)).getTitle());
+				temp.append("<Ksiazka> " + ((BookWrapper)m_subcategories.get(i)).getTitle());
 			else
 				temp.append(((Category)m_subcategories.get(i)).get_name());
 			
@@ -45,30 +47,38 @@ public class Category implements Browsable{
 	}
 	
 	public Browsable search(String category){
+		
+		if (category == "")
+			return this;
+		
 		String[] split_string = category.split("\\.");
 		String subcategory_name = split_string[0];
 		Integer subcategory_number = Integer.parseInt(subcategory_name);
 		String subcategory_string = category.replaceFirst(subcategory_name + "\\.", "");
 
-		return m_subcategories.get(subcategory_number).search(subcategory_string);
+		if (split_string.length > 1)
+			return m_subcategories.get(subcategory_number).search(subcategory_string);
+		else
+			return m_subcategories.get(subcategory_number);
 	}
 	
-	public void add_subcategory(String subcategory){
-		Category temp = new Category(subcategory);
+	public void add_subcategory(String name){
+		Category temp = new Category(name);
 		
 		m_subcategories.add(temp);
 	}
 	
 	public void add_book(int ID){
-		//go to bookshelf and get reference of book with id == ID
-		//m_subcategories.add() the book reference
-
-		System.out.println("Adding book");
+		
+		BookWrapper newBook = new BookWrapper();
+		newBook.set_ID(ID);
+		
+		m_subcategories.add(newBook);
 		
 	}
 	
-	public void remove(String subcategory){
-		m_subcategories.remove(subcategory);		
+	public void remove(int subcategory_number){
+		m_subcategories.remove(subcategory_number);		
 	}
 	
 	private List<Browsable> m_subcategories;
--- src/main/java/pl/edu/pk/pkCodeTeam1/biblioteka/ClassifiedCatalogue.java
+++ src/main/java/pl/edu/pk/pkCodeTeam1/biblioteka/ClassifiedCatalogue.java
@@ -11,12 +11,30 @@ package pl.edu.pk.pkCodeTeam1.biblioteka;
 public class ClassifiedCatalogue {
 	
 	public void add_book(String category, int ID){
+		if (m_category_hierarchy == null)
+			return;
+		
 		Browsable temp = m_category_hierarchy.search(category);
 		
 		if (temp.is_book())
 			return;	//moznaby tez rzucac wyjatek ?
 		
 		((Category)temp).add_book(ID);
+	}
+	
+	public void add_category(String category, String name){
+		if (m_category_hierarchy == null){
+			m_category_hierarchy = new Category(name);
+			return;
+		}
+		
+		Browsable temp = m_category_hierarchy.search(category);
+		
+		if (temp.is_book())
+			return;
+		
+		((Category)temp).add_subcategory(name);
+		
 	}
 	
 	public void print(String category){
