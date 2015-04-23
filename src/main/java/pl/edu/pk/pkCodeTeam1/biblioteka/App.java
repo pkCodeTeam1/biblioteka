6f969895030c5138a16830fca9f54968351e793d
Mateusz Niwa <niwa.mateusz@gmail.com>
2015-04-23 10:06:00 +0200

    Zmiana maina.

--- src/main/java/pl/edu/pk/pkCodeTeam1/biblioteka/App.java
+++ src/main/java/pl/edu/pk/pkCodeTeam1/biblioteka/App.java
@@ -1,20 +1,62 @@
 package pl.edu.pk.pkCodeTeam1.biblioteka;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static final String authors[]={"JanuszC", "AdamK", "JozefS", "StanislawW", "JuliuszS", "KrzysztofB"};
+	private static final String titles[]={"X", "XX", "XXX", "Y", "YY", "YYY", "Z", "ZZ", "ZZZ"};
+	private static final String categories[]={"A", "AA", "AAA", "B", "BB", "BBB", "C", "CC", "CCC"};
+	private static ClassifiedCatalogue katalog= new ClassifiedCatalogue();
+	
+	private static String getRandomCategory()
+	{
+		return categories[(int)(Math.random()*categories.length)];
+	}
+	
+	private static String getRandomCategoryNumber(int n)
+	{
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0 ; i < n ; ++i){
+			sb.append((int)Math.random()*3);
+			if (i < n-1)
+				sb.append(".");
+		}
+		
+		return sb.toString();
+	}
+	
+	
+	private static String getRandomAuthor()
+	{
+		return authors[(int)(Math.random()*authors.length)];
+	}
+	
+	private static String getRandomTitle()
+	{
+		return titles[(int)(Math.random()*titles.length)];
+	}
+	
     public static void main( String[] args )
     {
-        System.out.println( "Działanie karty czytelników: " );
-        ReaderCard prs1 = new ReaderCard("123", "Kamil", "Strózik");
-        ReaderCard prs2 = new ReaderCard("124", "Jan", "Kowalski");
-        System.out.println(prs1);
-        prs2.type_junior();
-        System.out.println(prs2);
-        prs1.show_fee();
-        prs2.show_penalty();
+
+    	for (int i=1; i<=20; i++){
+    			
+    		/*Book book=(Book)BookFactory.getBook(i);
+    		book.setAuthor(getRandomAuthor());
+    		book.setTitle(getRandomTitle());
+    		BookWrapper bookw = new BookWrapper();
+    		bookw.set_ID(i);*/
+    		//katalog.add_category(getRandomCategoryNumber())
+    		System.out.println(getRandomCategoryNumber(3));
+    			
+    	}
+        
     }
 }
